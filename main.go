f91e780acc210007056d9fc5eb2aa4daa5bd11f0
likhithkp <likhithkpdev@gmail.com>
2024-05-19 11:59:13 +0530

    Added logics

--- main.go
+++ main.go
@@ -33,18 +33,39 @@ func main() {
 		fmt.Println("Enter the number of tickets: ")
 		fmt.Scan(&userTickets)
 
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+    isValidName := len(firstName) >= 2 && len(lastName) >= 2
+    isValidEmail := strings.Contains(email, "@")
+    isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-		fmt.Printf("Thank you %v %v for booking %v tickets, you will receive the tickets on your email %v\n", firstName, lastName, userTickets, email)
+    if isValidName && isValidEmail && isValidTicketNumber {
+      remainingTickets = remainingTickets - userTickets
+		  bookings = append(bookings, firstName+" "+lastName)
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
+		  fmt.Printf("Thank you %v %v for booking %v tickets, you will receive the tickets on your email %v\n", firstName, lastName, userTickets,email)
 
-		fmt.Printf("Travelers list: %v\n", firstNames)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, placeName)
-	}
+		  firstNames := []string{}
+	  	for _, booking := range bookings {
+			  var names = strings.Fields(booking)
+			  firstNames = append(firstNames, names[0])
+		  }
+
+		  fmt.Printf("Travelers list: %v\n", firstNames)
+		  fmt.Printf("%v tickets remaining for %v\n", remainingTickets, placeName)
+
+      if remainingTickets == 0 {
+        fmt.Println("Tickets sold out, please come back next year")
+        break
+      }
+    } else {
+        if !isValidName {
+          fmt.Println("Invalid name, first name and last name must be more than 2 characters")
+        }
+        if !isValidEmail {
+          fmt.Println("Invalid email, please ensure that your email has @ in it")
+        }
+        if !isValidTicketNumber {
+          fmt.Printf("Invalid ticket counts, you can book between 1 to %v\n", remainingTickets)
+        }
+    }
+  }
 }
